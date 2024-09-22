0f597911d2f66e77a6e4513d7f0720e67556b03a
Lucas <junsuny789@gmail.com>
2024-09-21 20:12:36 -0400

    adding cmake, making minor changes to fix bad alloc bug

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 
-#include <ART.h>
+#include "ART.h"
 
 #include <chrono>
 #include <fstream>
@@ -14,16 +14,18 @@ std::vector<key_type> read_bin(const char* filename) {
     inputFile.seekg(0, std::ios::end);
     const std::streampos fileSize = inputFile.tellg();
     inputFile.seekg(0, std::ios::beg);
+
     std::vector<key_type> data(fileSize / sizeof(key_type));
+
     inputFile.read(reinterpret_cast<char*>(data.data()), fileSize);
+
     return data;
 }
 
 int main(int argc, char** argv) {
     bool verbose = false;  // optional argument
-    int N = 1000000;       // optional argument
+    int N = 5000000;       // optional argument
     string input_file;     // required argument
-
     // Parse arguments; make sure to increment i by 2 if you consume an argument
     for (int i = 1; i < argc;) {
         if (string(argv[i]) == "-v") {
@@ -33,7 +35,7 @@ int main(int argc, char** argv) {
             N = atoi(argv[i + 1]);
             i += 2;
         } else if (string(argv[i]) == "-f") {
-            input_file = argv[i];
+            input_file = argv[i+1];
             i += 2;
         }
     }
